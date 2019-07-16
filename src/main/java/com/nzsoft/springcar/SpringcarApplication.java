c32762a8166dd2ebf6da699422519ea4b58df547
unknown <profegranollers@gmail.com>
2019-07-16 21:15:29 +0200

    fuera syso de SpringApplication de cuando se empezó a probar git

--- src/main/java/com/nzsoft/springcar/SpringcarApplication.java
+++ src/main/java/com/nzsoft/springcar/SpringcarApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringcarApplication {
 
 	public static void main(String[] args) {
-		
-		System.out.println("pruebas git 3...");
-		
 		SpringApplication.run(SpringcarApplication.class, args);
 	}
 
