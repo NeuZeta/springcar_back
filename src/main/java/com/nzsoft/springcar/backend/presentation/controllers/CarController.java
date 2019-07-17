c5fce88696b0f49ca610b9f82f963843f76981d5
unknown <profegranollers@gmail.com>
2019-07-17 19:45:18 +0200

    controlador Car tiene endpoint para descargar fotos de coches

--- src/main/java/com/nzsoft/springcar/backend/presentation/controllers/CarController.java
+++ src/main/java/com/nzsoft/springcar/backend/presentation/controllers/CarController.java
@@ -1,9 +1,16 @@
 package com.nzsoft.springcar.backend.presentation.controllers;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +28,21 @@ public class CarController {
 			        produces=MediaType.APPLICATION_JSON_VALUE)
 	public List<Car> getAll(){
 		return carServices.getAll();
+	}
+	
+	@RequestMapping(value="/images/{carImage}",
+			method=RequestMethod.GET,
+			produces=MediaType.IMAGE_JPEG_VALUE)
+	public ResponseEntity<byte[]> getImageAsResource(@PathVariable ("carImage") String carImage) throws IOException{
+
+		ClassPathResource imgFile = new ClassPathResource("/img/" + carImage + ".png");
+
+		//Para recoger las imagenes del sistema de archivos (sería lo ideal)
+		//FileSystemResource imgFile = new FileSystemResource("c:\\imagenes\\chic1.jpg");
+
+		byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+
+		return ResponseEntity.ok().body(bytes);
 	}
 	
 }
