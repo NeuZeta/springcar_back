1b77a3207e2ab4a6100324edc59dd5dd76301397
NeuZeta <neuzeta@gmail.com>
2019-07-25 13:37:57 +0200

    add realistic data into db

--- src/main/resources/data/h2/data.sql
+++ src/main/resources/data/h2/data.sql
@@ -5,37 +5,74 @@ INSERT INTO SECUENCIAS VALUES
 ('EXTRA_SEQ', 1000);
 
 INSERT INTO CATEGORIES VALUES
-(1,'Categoría A',100,100,100),
-(2,'Categoría B',200,200,200),
-(3,'Categoría C',300,300,300);
+(1,'Economic',100,250,150),
+(2,'Standard',150,400,275),
+(3,'Top',300,850,680);
 
 
 INSERT INTO CLIENTS VALUES
-(10,'Cliente 1','Apellidos 1','99999999W','Calle 1','Zip code 1','City 1', 'Country 1','932211111','email1@email1.com','username 2', 'password 1'),
-(20,'Cliente 2','Apellidos 2','99999999X','Calle 2','Zip code 2','City 2', 'Country 2','932211112','email2@email2.com','username 2', 'password 2'),
-(30,'Cliente 3','Apellidos 3','99999999Y','Calle 3','Zip code 3','City 3', 'Country 3','932211113','email3@email3.com','username 3', 'password 3'),
-(40,'Cliente 4','Apellidos 4','99999999Z','Calle 4','Zip code 4','City 4', 'Country 4','932211114','email4@email4.com','username 4', 'password 4');
+(10,'Neus','Rodríguez','47646238J','Roger de Llúria, 23','08923','Santa Coloma de Gramanet', 'Spain','677313640','neuzeta@gmail.com','NeuZeta', 'neuzeta'),
+(20,'Florentino','Lopez','53645521N','Roger de Llúria, 23','08923','Santa Coloma de Gramanet', 'Spain','669854571','florentino.lopez@gmail.com','Flonchi77', 'flonchi77'),
+(30,'Magda','Baró','53698785F','Cervantes, 126','08943','Terrassa', 'Spain','660454321','mbaro@gmail.com','Magdala', 'magdala'),
+(40,'Marina','Tudó','48562355Z','Era, 16','08245','Terrassa', 'Spain','630454778','marina.tudo@gmail.com','Marinaina83', 'marinaina83');
 
 INSERT INTO OFFICES VALUES
-(10, 'Oficina 1', 'Calle 1','Zip code 1','City 1', 'Country 1','932211111','email1@email1.com'),
-(20, 'Oficina 2', 'Calle 2','Zip code 2','City 2', 'Country 2','932211112','email2@email2.com');
+(10, 'Barcerelona - Eixample', 'Carrer d Aragó, 54','08015','Barcelona', 'Spain','932211111','SP.bcn.eixample@sixt.com'),
+(20, 'Barcelona - Rambla de Catalunya', 'Rambla de Catalunya, 74','08007','Barcelona', 'Spain','932211112','SP.bcn.rblacatalunya@sixt.com'),
+(30, 'Barcelona - Paral.lel / Port', 'Carrer Palaudàries 5-7','08004','Barcelona', 'Spain','932211113','SP.bcn.parallel@sixt.com'),
+(40, 'Barcelona - Airport', 'Terminal 1 & 2 Llegadas','08820','El Prat de Llobregat', 'Spain','932211114','SP.bcn.airport@sixt.com');
 
 INSERT INTO CARS VALUES
-(10,'model10',10,1,'AUTO',TRUE,5,4,3,'car10',100),
-(11,'model11',10,1,'AUTO',TRUE,5,4,3,'car11',100),
-(12,'model12',10,2,'AUTO',TRUE,5,4,3,'car12',100),
-(13,'model13',10,1,'AUTO',TRUE,5,4,3,'car13',100),
-(14,'model14',20,3,'AUTO',TRUE,5,4,3,'car14',100),
-(15,'model15',20,1,'AUTO',TRUE,5,4,3,'car15',100),
-(16,'model16',10,2,'AUTO',TRUE,5,4,3,'car16',100),
-(17,'model17',10,1,'MANUAL',FALSE,5,4,3,'car17',100),
-(18,'model18',10,1,'AUTO',TRUE,5,4,3,'car18',100),
-(19,'model19',20,1,'AUTO',TRUE,5,4,3,'car19',100),
-(20,'model20',20,3,'AUTO',TRUE,5,4,3,'car20',100),
-(21,'model21',20,1,'AUTO',TRUE,5,4,3,'car21',100),
-(22,'model22',10,2,'AUTO',TRUE,5,4,3,'car22',100),
-(23,'model23',10,2,'AUTO',TRUE,5,4,3,'car23',100),
-(24,'model24',10,1,'AUTO',TRUE,5,4,3,'car24',100);
+(10,'Audi A5 Cabrio',10,3,'MANUAL',TRUE,4,3,2,'car10',235.80),
+(11,'Audi A5 Cabrio',20,3,'MANUAL',TRUE,4,3,2,'car10',235.80),
+(12,'Audi A5 Cabrio',30,3,'MANUAL',TRUE,4,3,2,'car10',235.80),
+(13,'Audi A5 Cabrio',40,3,'MANUAL',TRUE,4,3,2,'car10',235.80),
+(14,'Audi A6 Avant',10,3,'AUTO',TRUE,5,5,2,'car11',257.99),
+(15,'Audi A6 Avant',20,3,'AUTO',TRUE,5,5,2,'car11',257.99),
+(16,'Audi TT',20,3,'MANUAL',TRUE,5,4,3,'car12',246.60),
+(17,'Audi TT',30,3,'MANUAL',TRUE,5,4,3,'car12',246.60),
+(18,'Audi TT',40,3,'MANUAL',TRUE,5,4,3,'car12',246.60),
+(19,'BMW Serie 2 Active Tourer',20,3,'AUTO',TRUE,5,5,2,'car13',246.60),
+(20,'BMW Serie 3 SW',20,3,'MANUAL',TRUE,5,5,2,'car14',279.99),
+(21,'Citroën C4 Cactus',10,2,'AUTO',TRUE,5,5,1,'car15',150.60),
+(22,'Citroën C4 Cactus',20,2,'AUTO',TRUE,5,5,1,'car15',150.60),
+(23,'Citroën C4 Cactus',30,2,'AUTO',TRUE,5,5,1,'car15',150.60),
+(24,'Citroën C4 Cactus',40,2,'AUTO',TRUE,5,5,1,'car15',150.60),
+(25,'Fiat 500 Convertible',10,3,'MANUAL',TRUE,2,4,1,'car16',229.99),
+(26,'Fiat 500 Convertible',20,3,'MANUAL',TRUE,2,4,1,'car16',229.99),
+(27,'Fiat 500 Convertible',30,3,'MANUAL',TRUE,2,4,1,'car16',229.99),
+(28,'Fiat 500 Convertible',40,3,'MANUAL',TRUE,2,4,1,'car16',229.99),
+(29,'BMW Serie 5',10,3,'MANUAL',TRUE,5,5,2,'car17',289.99),
+(30,'Ford Ka',10,1,'MANUAL',TRUE,4,3,2,'car18',75.20),
+(31,'Ford Ka',20,1,'MANUAL',TRUE,4,3,2,'car18',75.20),
+(32,'Ford Ka',30,1,'MANUAL',TRUE,4,3,2,'car18',75.20),
+(33,'Ford Ka',40,1,'MANUAL',TRUE,4,3,2,'car18',75.20),
+(34,'Jaguar F-Type Coupé',20,3,'AUTO',TRUE,2,3,0,'car19',325.99),
+(35,'Jeep Wrangler Cabrio',20,2,'AUTO',TRUE,4,3,1,'car20',270.15),
+(36,'BMW X1',20,3,'AUTO',TRUE,5,5,1,'car21',235.80),
+(37,'BMW 550d M Package',10,3,'AUTO',TRUE,5,5,2,'car22',279.99),
+(38,'BMW M850i Coupé',10,3,'AUTO',TRUE,4,2,1,'car23',289.99),
+(39,'BMW M850i Cabrio',10,3,'MANUAL',TRUE,2,2,0,'car24',289.99),
+(40,'Opel Corsa',10,1,'MANUAL',TRUE,5,5,1,'car25',76.77),
+(41,'Opel Corsa',20,1,'MANUAL',TRUE,5,5,1,'car25',76.77),
+(42,'Opel Corsa',30,1,'MANUAL',TRUE,5,5,1,'car25',76.77),
+(43,'Opel Corsa',40,1,'MANUAL',TRUE,5,5,1,'car25',76.77),
+(44, 'Peugeot 308',10,1,'MANUAL',TRUE,5,5,1,'car26',84.35),
+(45, 'Peugeot 308',20,1,'MANUAL',TRUE,5,5,1,'car26',84.35),
+(46, 'Peugeot 308',30,1,'MANUAL',TRUE,5,5,1,'car26',84.35),
+(47, 'Peugeot 308',40,1,'MANUAL',TRUE,5,5,1,'car26',84.35),
+(48, 'Smart Fortwo',10,1,'AUTOMATICO',TRUE,2,3,0,'car27', 79.99),
+(49, 'Smart Fortwo',20,1,'AUTOMATICO',TRUE,2,3,0,'car27', 79.99),
+(50, 'Smart Fortwo',30,1,'AUTOMATICO',TRUE,2,3,0,'car27', 79.99),
+(51, 'Smart Fortwo',40,1,'AUTOMATICO',TRUE,2,3,0,'car27', 79.99),
+(52, 'Smart Forfour',10,1,'MANUAL', TRUE,4,4,1,'car28', 85.40),
+(53, 'Smart Forfour',20,1,'MANUAL', TRUE,4,4,1,'car28', 85.40),
+(54, 'Smart Forfour',30,1,'MANUAL', TRUE,4,4,1,'car28', 85.40),
+(55, 'Smart Forfour',40,1,'MANUAL', TRUE,4,4,1,'car28', 85.40),
+(56, 'Toyota Yaris Hybrid',10,2,'AUTO',TRUE,4,5,1,'car29', 135.60),
+(57, 'Toyota Yaris Hybrid',20,2,'AUTO',TRUE,4,5,1,'car29', 135.60),
+(58, 'Mercedes-Benz Clase C',10,3,'MANUAL',TRUE,5,5,2,'car30',289.99);
+
 
 INSERT INTO RESERVATIONS VALUES
 (1,'2019-01-20 10:50:00','2019-10-30 09:00:00','2019-10-30 21:00:00',10,10,'BASE',TRUE),
